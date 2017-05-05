5aaf09560491b1158ab58cc47ea1c32692069bd6
Jonas Wingård <jonas.wingard@gmail.com>
2017-05-05 09:58:28 +0200

    lab with webrtc

---
+++ webrtc/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var errorElement = document.querySelector('#errorMsg');
+var video = document.querySelector('video');
+
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  var videoTracks = stream.getVideoTracks();
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  errorMsg('getUserMedia error: ' + error.name, error);
+}
+
+function errorMsg(msg, error) {
+  errorElement.innerHTML += '<p>' + msg + '</p>';
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
